7521ccef8fe2269a59eef5e9a8c65939592b628a
andy <rjzjh@163.com>
2020-08-16 06:58:09 +0800

    决定使用plus

--- duckula-controller/src/main/java/net/wicp/tams/app/duckula/controller/web/task/QueryTask.java
+++ duckula-controller/src/main/java/net/wicp/tams/app/duckula/controller/web/task/QueryTask.java
@@ -20,6 +20,8 @@ public class QueryTask implements IBusiApp {
 	public CusDynaBean exe(CusDynaBean inputBean, CusDynaBean outBeanOri) throws ProjectException {
 		String name = inputBean.getStrValueByName("name");
 		CommonTask commonTask = commonTaskMapper.selectById(1);
+		CommonTask selectByPrimaryKey = commonTaskMapper.selectByPrimaryKey(1);
+		System.out.println(selectByPrimaryKey.getName());
 		Result ret = Result.getSuc(commonTask.getName());
 		outBeanOri.setResult(ret);
 		outBeanOri.set("ret1", "hello " + name);
