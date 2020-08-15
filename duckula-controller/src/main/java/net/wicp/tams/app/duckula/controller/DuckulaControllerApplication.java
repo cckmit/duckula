9494e9a85935ab260d0d6335f7c2bbb856f2b657
andy <rjzjh@163.com>
2020-08-15 12:02:33 +0800

    没有工具类

--- duckula-controller/src/main/java/net/wicp/tams/app/duckula/controller/DuckulaControllerApplication.java
+++ duckula-controller/src/main/java/net/wicp/tams/app/duckula/controller/DuckulaControllerApplication.java
@@ -1,5 +1,6 @@
 package net.wicp.tams.app.duckula.controller;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource("classpath:beanRefContext.xml")
 @Configuration
+@MapperScan("net.wicp.tams.app.duckula.controller.dao")
 public class DuckulaControllerApplication {
 
 	public static void main(String[] args) {
--- duckula-controller/src/main/java/net/wicp/tams/app/duckula/controller/web/task/QueryTask.java
+++ duckula-controller/src/main/java/net/wicp/tams/app/duckula/controller/web/task/QueryTask.java
@@ -1,7 +1,10 @@
 package net.wicp.tams.app.duckula.controller.web.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import net.wicp.tams.app.duckula.controller.bean.models.CommonTask;
+import net.wicp.tams.app.duckula.controller.dao.CommonTaskMapper;
 import net.wicp.tams.common.Result;
 import net.wicp.tams.common.connector.beans.CusDynaBean;
 import net.wicp.tams.common.connector.executor.IBusiApp;
@@ -10,13 +13,13 @@ import net.wicp.tams.common.exception.ProjectException;
 @Service(value = "task.QueryTask")
 public class QueryTask implements IBusiApp {
 
-	//@Autowired
-	//private CommonTaskMapper commonTaskMapper;
+	@Autowired
+	private CommonTaskMapper commonTaskMapper;
 
 	@Override
 	public CusDynaBean exe(CusDynaBean inputBean, CusDynaBean outBeanOri) throws ProjectException {
 		String name = inputBean.getStrValueByName("name");
-		//CommonTask commonTask = commonTaskMapper.selectByPrimaryKey(1);
+		CommonTask commonTask = commonTaskMapper.selectByPrimaryKey(1);
 		Result ret = Result.getSuc("");
 		outBeanOri.setResult(ret);
 		outBeanOri.set("ret1", "hello " + name);
