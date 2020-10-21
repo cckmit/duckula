1be18e907487fd04d888cec5859c125a602fc932
andy <rjzjh@163.com>
2020-10-21 10:01:34 +0800

    加入mybatis

--- duckula-ops/src/main/java/net/wicp/tams/duckula/ops/App.java
+++ duckula-ops/src/main/java/net/wicp/tams/duckula/ops/App.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "net.wicp.tams.app.duckula.controller","net.wicp.tams.duckula.ops"})
 public class App extends SpringBootServletInitializer
 {
 
