cfd6ec9961df2abebc949ef8fe3d2a9108fc07c7
andy <rjzjh@163.com>
2020-10-26 19:44:35 +0800

    docker运行

--- duckula-ops/src/main/java/net/wicp/tams/duckula/ops/App.java
+++ duckula-ops/src/main/java/net/wicp/tams/duckula/ops/App.java
@@ -9,26 +9,25 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
+import net.wicp.tams.common.Conf;
 import net.wicp.tams.common.spring.autoconfig.annotation.EnableTams;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "net.wicp.tams.app.duckula.controller","net.wicp.tams.duckula.ops"})
+@ComponentScan(basePackages = { "net.wicp.tams.app.duckula.controller", "net.wicp.tams.duckula.ops" })
 @ImportResource("classpath:beanRefContext.xml")
 @MapperScan("net.wicp.tams.app.duckula.controller.dao")
 @EnableTams
-public class App extends SpringBootServletInitializer
-{
+public class App extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
-    {
-        return application.sources(AppConfiguration.class);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppConfiguration.class);
+	}
 
-    public static void main(String[] args) throws Exception
-    {
-        SpringApplication application = new SpringApplication(App.class);
-        application.setApplicationContextClass(AnnotationConfigWebApplicationContext.class);
-        SpringApplication.run(App.class, args);
-    }
+	public static void main(String[] args) throws Exception {
+		Conf.overProp("common.spring.autoconfig.property.path", "abs:/data/duckula-data/conf/application.properties");
+		SpringApplication application = new SpringApplication(App.class);
+		application.setApplicationContextClass(AnnotationConfigWebApplicationContext.class);
+		SpringApplication.run(App.class, args);
+	}
 }
